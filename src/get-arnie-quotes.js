92982b3133d7a1535e528f9e62fe807eaa36cf7a
Karol Tarasiuk <ktarasiuk@atlassian.com>
2021-04-18 18:38:26 +1000

    getArnieQuotes implementation

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,15 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  return Promise.all(
+    urls.map((url) =>
+      httpGet(url).then((response) =>
+        response.status === 200
+          ? { "Arnie Quote": JSON.parse(response.body).message }
+          : { FAILURE: "Your request has been terminated" }
+      )
+    )
+  );
 };
 
 module.exports = {
